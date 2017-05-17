1a68d143785abad1366907b08ff448e52eab2afd
Red27 <luntungle@mail.ru>
2017-05-17 12:27:56 +0300

    change color order

--- src/Products/Details/Header/ColorSelector.jsx
+++ src/Products/Details/Header/ColorSelector.jsx
@@ -2,29 +2,30 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-display: inline-block;
-position: relative;
-top: 4px;
-margin-right: 20px;
-
+display: flex;
+justify-content: space-between;
+width: 111px;
+{''/* top: 4px;
+margin-right: 20px; */}
 `;
 
 const Button = styled.button`
   border: none;
-  border-radius: 100%;
+  outline: none;
+  cursor: pointer;
+  border-radius: 50%;
   width: 18px;
   height: 18px;
   margin: 0 3px;
-  cursor: pointer;
-  background-color: ${p => (p.color ? `${p.color};` : '#e0e0e0;')}
-  margin-right: 13px;
+  background-color: ${props => props.color};
+
 `;
 
 export default () => (
   <Wrapper>
-    <Button />
     <Button color="#c5c5c5" />
     <Button color="#4d87ca" />
     <Button color="#000000" />
+    <Button color="#e0e0e0" />
   </Wrapper>
 );
