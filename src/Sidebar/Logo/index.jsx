e63e560d428ee0027561c52026d2ab7a8b1900be
luntungle <luntungle@mail.ru>
2017-05-22 21:59:59 +0300

    react Link for logo

--- src/Sidebar/Logo/index.jsx
+++ src/Sidebar/Logo/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from './adidaslogo.png';
 
-const Img = styled.a`
+const Img = styled.img`
   display: flex;
   justify-content: center;
   margin-top: 31px;
 `;
 
 export default () => (
-  <Img href="http://www.adidas.com">
-    <img src={logo} alt="logo" />
-  </Img>
+  <Link to="/">
+    <Img src={logo} alt="logo" />
+  </Link>
 );
