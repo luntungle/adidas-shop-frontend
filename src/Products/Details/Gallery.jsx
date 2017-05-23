51bccb88b43bc17398436ed13a8fde1e9520795c
luntungle <luntungle@mail.ru>
2017-05-23 16:51:14 +0300

    remove blocks 'more photos'

--- src/Products/Details/Gallery.jsx
+++ src/Products/Details/Gallery.jsx
@@ -22,46 +22,12 @@ const Thumb = styled.img`
   &:hover { outline: 4px solid #eaeaec }
 `;
 
-const ThreeBars = styled.div`
-  margin: 0 22px;
-  cursor: pointer;
-  border: none;
-  outline: none;
-  background: none;
-`;
-
-const Bar = styled.hr`
-  margin: 3px;
-  width: 159px;
-  height: 3px;
-  border-radius: 1.5px;
-  border: none;
-  background-color: #e7e7e7;
-`;
-
-const ButtonMore = styled.button`
-  flex: 0 1 40px;
-  width: 64px;
-  text-align: left;
-  font-family: 'avenir-next';
-  font-size: 14px;
-  font-weight: bold;
-  color: #c0c0c0;
-  cursor: pointer;
-  outline: none;
-  border: none;
-  background: transparent;
-`;
-
 export default () => (
   <Gallery>
     <Thumb src={thumb1} alt="" />
     <Thumb src={thumb2} alt="" />
     <Thumb src={thumb3} alt="" />
     <Thumb src={thumb4} alt="" />
-    <ThreeBars>
-      <Bar /> <Bar /> <Bar />
-    </ThreeBars>
-    <ButtonMore>see more photos</ButtonMore>
+    <Thumb src={thumb2} alt="" />
   </Gallery>
 );
